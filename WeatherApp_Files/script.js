a82c7d8aa6ecde1c7634681e86d20d29ce84d299
Nazar_Frazovir <frazovirnazar@gmail.com>
2024-11-15 03:24:25 +0200

    Add weather icon.

--- WeatherApp_Files/script.js
+++ WeatherApp_Files/script.js
@@ -1,12 +1,12 @@
 async function getWeather() {
     const city = document.getElementById("city").value;
     const apiKey = "4f70c532cd37606000c3d68f88ae0e24"; 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=uk`;
   
     try {
       const response = await fetch(apiUrl);
       if (!response.ok) {
-        throw new Error("Місто не знайдено або помилка підключення до OpenWeatherMap API");
+        throw new Error("Місто не знайдено");
       }
   
       const data = await response.json();
@@ -21,4 +21,12 @@ async function getWeather() {
     document.getElementById("temperature").innerText = `Температура: ${data.main.temp}°C`;
     document.getElementById("description").innerText = `Опис: ${data.weather[0].description}`;
     document.getElementById("humidity").innerText = `Вологість: ${data.main.humidity}%`;
+
+    // Додаємо іконку погоди
+  const iconCode = data.weather[0].icon;
+  const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  const weatherIcon = document.getElementById("weather-icon");
+
+  weatherIcon.src = iconUrl;
+  weatherIcon.style.display = "block"; // Показуємо іконку
   }  
